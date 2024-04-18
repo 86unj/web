10be7ddd7e1bda677b757988ecb74c40f753ec09
Yunjeong Choi <choi1@myseneca.ca>
2024-04-18 15:46:03 -0400

    calculator v1

---
+++ caculator/cal.js
@@ -0,0 +1,42 @@
+let elementsArray = document.querySelectorAll(".number-button");
+let operatorArray = document.querySelectorAll(".operator-button");
+let screenField = document.getElementById("screen");
+let numberArray=[];
+let i=0;
+let Anumber="";
+let result;
+
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("click", function(){
+        numberArray[i]=elem.textContent;
+        i++;
+        Anumber += elem.textContent;
+        screenField.innerHTML = Anumber;
+    });
+});
+
+operatorArray.forEach(function(oper) {
+       oper.addEventListener("click", function(){
+        let firstNumber = Number(Anumber);
+        screenField.innerHTML="";
+        Anumber="";
+        numberArray[i]=oper.textContent;
+        i++;
+        });
+});
+
+document.getElementById("equal").addEventListener("click", function() {
+        result=eval(numberArray.join(""));
+        screenField.innerHTML = result;
+        numberArray=[];
+        Anumber="";
+        i=0;
+});
+
+document.getElementById("clear").addEventListener("click", function(){
+        numberArray=[];
+        Anumber=""
+        screenField.innerHTML=""
+        i=0;
+})
