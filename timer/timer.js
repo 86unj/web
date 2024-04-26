b6a203b149615bedd9f4d67983a30a5a5994b6c2
Yunjeong Choi <choi1@myseneca.ca>
2024-04-26 13:39:35 -0400

    stop time when it beomes zero

--- timer/timer.js
+++ timer/timer.js
@@ -1,34 +1,37 @@
 let second = document.getElementById("sec");
 let minute = document.getElementById("min");
 let stopButton = document.getElementById("stop");
-let time = 5 * 60;
+let time = 10;
 
-if (time <= 0) {
-    clearInterval}
-
-document.getElementById("start").addEventListener("click", function(){  
-        thisInterval = setInterval(function () {
-            time -= 1
-            second.innerHTML=time%60;
-            minute.innerHTML=parseInt(time/60);
-        }, 1000);
+document.getElementById("start").addEventListener("click", function () {
+    thisInterval = setInterval(function() { 
+        time -= 1
+        second.innerHTML = time % 60;
+        minute.innerHTML = parseInt(time / 60);
+        if (time ===0){
+            clearInterval(thisInterval)
+            alert("Time is done!")
+        }
+    }
+        , 1000);
+    
 });
 
-stopButton.addEventListener("click", function() {
-    if (stopButton.innterHTML == "stop"){
-    clearInterval(thisInterval);
-    stopButton.innerHTML = "resume"
-    }
-    else {
-        stopButton.innerHTML = "stop"
-    }
+stopButton.addEventListener("click", function () {
+        if (stopButton.innerHTML == "stop"){
+            clearInterval(thisInterval)
+            stopButton.innerHTML = "resume"}
+        else{
+            stopButton.innerHTML ="stop"
+            thisInterval;
+        }
 });
 
-document.getElementById("reset").addEventListener("click", function() {
+document.getElementById("reset").addEventListener("click", function () {
     clearInterval(thisInterval);
-    time = 5*60
-    second.innerHTML = time%60;
-    min.innerHTML = parseInt(time/60);
+    time = 5 * 60
+    second.innerHTML = time % 60;
+    min.innerHTML = parseInt(time / 60);
 });
 
 
