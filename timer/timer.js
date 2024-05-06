b094bf86249ee3b2dfc8bf4403238c9bdc374209
Yunjeong Choi <choi1@myseneca.ca>
2024-05-06 13:45:16 -0400

    progress bar work

--- timer/timer.js
+++ timer/timer.js
@@ -1,45 +1,56 @@
 let second = document.getElementById("sec");
 let minute = document.getElementById("min");
 let stopButton = document.getElementById("stop");
-let time = 10;
+let progress = document.getElementById("bar-progress");
+let progressWidth = progress.offsetWidth;
+let time = 0;
 
 let userInput = 5;
-let userTime = userInput* 60;
+let userTime = userInput * 60;
 
 
-function timer() { 
+function timer() {
     time -= 1
     second.innerHTML = time % 60;
     minute.innerHTML = parseInt(time / 60);
-    if (time ===0){
+    decreaseProgress();
+    if (time === 0) {
         clearInterval(thisInterval)
-        document.getElementById("start").disabled=false;
+        document.getElementById("start").disabled = false;
     }
 }
 
 
 document.getElementById("start").addEventListener("click", function () {
-time = userTime;
-document.getElementById("start").disabled=true;
-    thisInterval = setInterval(timer , 1000);// why it doesn't walk with timer()
+    time = userTime;
+    document.getElementById("start").disabled = true;
+    thisInterval = setInterval(timer, 1000);// why it doesn't walk with timer()
 });
 
 stopButton.addEventListener("click", function () {
-        if (stopButton.innerHTML == "stop"){
-            clearInterval(thisInterval)
-            stopButton.innerHTML = "resume"}
-        else{
-            stopButton.innerHTML ="stop"
-            thisInterval= setInterval(timer , 1000);
-        }
-});
+    if (stopButton.innerHTML == "stop") {
+        clearInterval(thisInterval)
+        stopButton.innerHTML = "resume"
+    }
+    else {
+        stopButton.innerHTML = "stop"
+        thisInterval = setInterval(timer, 1000);
+    }
+}); 
 
 document.getElementById("reset").addEventListener("click", function () {
     clearInterval(thisInterval);
     time = 5 * 60
     second.innerHTML = time % 60;
     min.innerHTML = parseInt(time / 60);
-    document.getElementById("start").disabled=false;
+    document.getElementById("start").disabled = false;
+    progress.style.width=550+'px';
+    progressWidth=progress.offsetWidth;
 });
 
-
+function decreaseProgress(){
+    let decrementValue;
+    decrementValue= 550 / time
+    progressWidth -= decrementValue;
+    progress.style.width = progressWidth+'px';
+}
